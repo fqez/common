e40b35af737bba9948ef666aae6879a721e1df66
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2016-07-15 10:19:13 +0200

    commented repository that make problems with boost instalation

--- jails/ubuntu/setup.sh
+++ jails/ubuntu/setup.sh
@@ -9,7 +9,8 @@ echo 'apt-get -y install software-properties-common nano' >> $filename
 echo 'ver=$(lsb_release -cs)'  >> $filename
 
 echo 'echo deb http://es.archive.ubuntu.com/ubuntu/ $ver universe >> /etc/apt/sources.list'  >> $filename
-echo 'echo deb http://es.archive.ubuntu.com/ubuntu/ $ver-updates universe >> /etc/apt/sources.list' >> $filename
+#da problemas al instalar boost
+#echo 'echo deb http://es.archive.ubuntu.com/ubuntu/ $ver-updates universe >> /etc/apt/sources.list' >> $filename
 
 echo 'echo deb http://es.archive.ubuntu.com/ubuntu/ $ver multiverse >> /etc/apt/sources.list' >> $filename
 echo 'echo deb http://es.archive.ubuntu.com/ubuntu/ $ver-updates multiverse >> /etc/apt/sources.list' >> $filename
@@ -21,6 +22,15 @@ echo 'apt-key adv --keyserver keyserver.ubuntu.com --recv 3E5C1192' >> $filename
 echo 'apt-get update' >> $filename
 
 echo 'apt-get -y install xauth' >> $filename
+
+echo 'echo "# enable bash completion in interactive shells" >> /etc/bash.bashrc' >> $filename
+echo 'echo "if ! shopt -oq posix; then">> /etc/bash.bashrc' >> $filename
+echo 'echo "  if [ -f /usr/share/bash-completion/bash_completion ]; then">> /etc/bash.bashrc' >> $filename
+echo 'echo "  . /usr/share/bash-completion/bash_completion">> /etc/bash.bashrc' >> $filename
+echo 'echo " elif [ -f /etc/bash_completion ]; then">> /etc/bash.bashrc' >> $filename
+echo 'echo "   . /etc/bash_completion">> /etc/bash.bashrc' >> $filename
+echo 'echo "fi">> /etc/bash.bashrc' >> $filename
+echo 'echo "fi">> /etc/bash.bashrc' >> $filename
 
 sudo schroot -c $1 --directory / -u root /bin/bash "$filename"
 
