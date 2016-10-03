86116880f9a42812206b8b45b7622cae097d2847
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2016-10-03 09:16:00 +0200

    -t en runjain para usarlo mediante ssh

--- jails/installjails.sh
+++ jails/installjails.sh
@@ -6,10 +6,9 @@ if [ ! -d /etc/schroot ]; then
 
     sudo sed -i "s|^/home|#/home|g" /etc/schroot/default/fstab
 fi
-if [ ! -d $jailsDest/jails ]; then
-    sudo cp -r jails $jailsDest
-    sudo cp bin/* /usr/local/bin
+
+sudo cp -r jails $jailsDest
+sudo cp bin/* /usr/local/bin
     #exp="export jails=$jailsDest/jails"
     #$exp
     #echo $exp >> ~/.bashrc
-fi
